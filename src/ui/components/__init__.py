3f32c146c8c24b1e94d3113be979b1c5525f3927
Sateesh Ambesange ( PragyanAI) <75975970+pragyanaischool@users.noreply.github.com>
2026-08-29 22:20:55 +0530

    Add file uploader to component exports

--- src/ui/components/__init__.py
+++ src/ui/components/__init__.py
@@ -7,6 +7,7 @@ Reusable Streamlit UI widgets and components.
 from src.ui.components.auth_widget import render_auth_sidebar
 from src.ui.components.chat_interface import render_multimodal_chat
 from src.ui.components.cutoff_explorer import render_cutoff_finder
+from src.ui.components.file_uploader import render_document_uploader
 from src.ui.components.roi_charts import render_roi_analytics_dashboard
 
 __all__ = [
@@ -14,4 +15,5 @@ __all__ = [
     "render_multimodal_chat",
     "render_cutoff_finder",
     "render_roi_analytics_dashboard",
+    "render_document_uploader",
 ]
