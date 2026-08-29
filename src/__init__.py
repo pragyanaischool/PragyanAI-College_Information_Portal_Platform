fc7179fa17371a276cee4008a1b50d12c0165cfe
Sateesh Ambesange ( PragyanAI) <75975970+pragyanaischool@users.noreply.github.com>
2026-08-29 18:15:16 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""
+PragyanAI College Intelligence Hub
+Enterprise Agentic Decision-Support Platform for Engineering & Higher Education Institutions.
+"""
+
+__version__ = "1.0.0"
+__author__ = "PragyanAI Engineering"
